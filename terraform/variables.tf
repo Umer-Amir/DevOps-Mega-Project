539b6b8f473376600c78924f8faf5f9153d99a47
Umer Amir <bscs2112241@szabist.pk>
2025-06-16 08:09:20 +0500

    Add Terraform configuration files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,10 +25,31 @@ variable "admin_username" {
 variable "public_key_path" {
   description = "Path to the public SSH key"
   type        = string
+  default     = "../jenkins_ssh_key.pub"
 }
 
 variable "vm_size" {
   description = "Size of the VM"
   type        = string
   default     = "Standard_B1s"
+}
+
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
 }
